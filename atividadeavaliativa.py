b03b9b0b1fcef6db91f17ae7d1b8e1f137f149f3
kawannerodrigues-crypto <kawanne.rodrigues@escola.pr.gov.br>
2026-06-17 09:23:54 -0300

    Add files via upload
    ultima versao

---
+++ atividadeavaliativa.py
@@ -0,0 +1,17 @@
+#desafio
+
+#Crie uma calculadora de Elegibilidada para um benefício social, como o Auxílio Jovem Aprendiz.
+
+# O programa deve ter:
+# Coletar dados do usuário
+#Verificar os critérios exigidos
+#informar se a pessoa pode ou não receber o benefício
+
+
+nome = input("Digite seu nome:")
+idade =int (input("digite sua idade:"))
+cidade = input("Digite sua cidade:")
+renda_mensal = (input("digite sua renda mensal:"))
+status= input("Digite seu status(estudante, trabalhador ou ambos):")
+
+
